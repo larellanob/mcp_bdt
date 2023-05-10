c2956c99c5d4839f613c03a01c145f9f4221e86e
Luciano Arellano <luciano.arellano@manchester.ac.uk>
2023-05-10 16:45:20 +0100

    completely redone published limits plotting tool

--- sensitivity/plot_sensitivity.cxx
+++ sensitivity/plot_sensitivity.cxx
@@ -1,179 +1,156 @@
-void fill_tgraph_vector(TString dir,   std::vector<TGraph *> &tgraphs);
-
-double get_epsilon(double v0, int nhits)
-{
-  double exponent = 2*nhits+2;
-  double powe0 = pow(0.001,exponent); // epsilon ^ 2nhits+2
-  //double nevents {15.032}; ///////////////////// <<<<< 15 EVENT LIMIT
-  double nevents = 1;
-  return pow(powe0*nevents*v0,1./(exponent));
-  //return v0;
-}
-
-double get_epsilon(double v0, int nhits, double mass)
-{
-  double exponent = 2*nhits+2;
-  double powe0 = pow(0.001,exponent); // epsilon ^ 2nhits+2
-  double nevents {15.032}; ///////////////////// <<<<< 15 EVENT LIMIT
-  return pow(powe0*nevents*v0,1./(exponent));   
-}
-
-void plot_sensitivity(int threshold = 1000)
-{
-
-  // published data
-  const char* dir = "/home/luciano/Physics/manchester/mcp_bdt/sensitivity/published/";
-  std::vector<TGraph *> tgraphs;
-  std::cout << "fill 1 " << std::endl;
-  fill_tgraph_vector(dir,tgraphs);  
-
-  // own limits
-  //const char* dir_sim = Form("/uboone/app/users/arellano/bdt_mcp/mass_scan/sensitivity_2206_with_milliqan/simulated/%ikev/",threshold);
-  const char* dir_sim = Form("./limits_apr_23/");
-  std::vector<TGraph *> tgraphs_sim;
-  std::cout << "fill 2 " << std::endl;
-  fill_tgraph_vector(dir_sim,tgraphs_sim);
-  
-  auto * c1 = new TCanvas();
-  c1->SetLogx();
-  c1->SetLogy();
-
-  
-  //tgraphs[1]->SetLineColor(kBlue);
-  //tgraphs[1]->SetLineStyle(2);
-  //tgraphs[1]->SetLineWidth(5);
-
-
-  tgraphs[5]->SetLineColor(kTeal);
-  tgraphs[5]->SetLineWidth(5);
-
-  tgraphs[4]->SetLineColor(kGray);
-  tgraphs[4]->SetLineWidth(3);
-
-  tgraphs[1]->SetLineColor(kRed);
-  tgraphs[1]->SetLineWidth(3);
-
-  tgraphs[0]->SetLineColor(kViolet);
-  tgraphs[0]->SetLineWidth(3);
-
-  tgraphs[2]->SetLineColor(kBlack);
-  tgraphs[2]->SetLineWidth(3);
-
-  tgraphs[3]->SetLineColor(kBlue);
-  tgraphs[3]->SetLineWidth(3);
-
-  tgraphs[1]->SetTitle("SLAC");
-  //tgraphs[1]->SetTitle("MicroBooNE (estimate using BNB)");
-  tgraphs[5]->SetTitle("ArgoNeuT");
-  tgraphs[4]->SetTitle("LHC");
-  tgraphs[0]->SetTitle("MiniBooNE");
-  tgraphs[2]->SetTitle("LSND");
-  tgraphs[3]->SetTitle("milliQan demonstrator");
-
-  gStyle->SetOptStat(0);
-
-  for ( size_t gr{0}; gr < tgraphs_sim.size(); gr++ ) {
-    tgraphs_sim[gr]->SetLineColor(kGreen+gr+1);
-    tgraphs_sim[gr]->SetLineWidth(3);
-    tgraphs_sim[gr]->SetLineStyle(3+gr);
-    tgraphs_sim[gr]->SetFillColorAlpha(kGreen+gr+1,0.2);
+class limit_curve {
+private:
+  ifstream dat_file;
+  TString dat_filename;
+  void fill_tgraph();
+  void read_file();
+  std::vector<double> x_points;
+  std::vector<double> y_points;
+  int npoints;
+  TGraph *limit;
+public:
+  limit_curve();
+  limit_curve(TString exp, Color_t col = kBlack, Bool_t pub = true);
+  ~limit_curve() { delete limit;};
+  TString experiment {"Experiment"};
+  Color_t color {kBlack};
+  Bool_t published {true}; // published by others as opposed to your own
+  Bool_t filled {false};
+  void set_exp(TString exp) {experiment = exp;};
+  void set_color(Color_t col) {color = col; limit->SetLineColor(color);};
+  void set_fill(Bool_t fill) {filled = fill;};
+  TGraph * get_tgraph() { return limit; };
+  void print_tgraph();
+  // overloads from ROOT
+  void SetFillColorAlpha(Color_t col, float alpha) {
+    this->get_tgraph()->SetFillColorAlpha(col, alpha);
   }
+  void Draw(const char * kOptions = "" ) {
+    this->get_tgraph()->Draw(kOptions);
+  }
+  void SetTitle(const char * title = "") {
+    this->get_tgraph()->SetTitle(title);
+  }
+  void SetLineStyle(int l) {
+    this->get_tgraph()->SetLineStyle(l);
+  }
+};
 
-  
+// main
+void plot_sensitivity()
+{
+  // base canvas
+  TCanvas *c1 = new TCanvas();
   TH2 * h2 = new TH2F("h2",";Mass m_{#chi} (MeV);Millicharge #epsilon = Q/e",10,10,10000,10,0.5e-4,2.5e-1);
   h2->GetXaxis()->CenterTitle();
   h2->GetYaxis()->CenterTitle();
+  c1->SetLogx();
+  c1->SetLogy();
   h2->Draw();
-  for ( size_t gr{0}; gr < tgraphs.size(); gr++ ) {
-    tgraphs[gr]->Draw("same");
-  }
-  for ( size_t gr{0}; gr < tgraphs_sim.size(); gr++ ) {
-    tgraphs_sim[gr]->Draw("same FL"); // F = filled, L = line
-  }
 
-  TLegend *myleg = new TLegend(0.6,0.15,0.9,0.45);
-  myleg->AddEntry(tgraphs[0]);
-  myleg->AddEntry(tgraphs[1]); // pheno estimate
-  myleg->AddEntry(tgraphs[2]);
-  myleg->AddEntry(tgraphs[3]);
-  myleg->AddEntry(tgraphs[4]);
-  myleg->AddEntry(tgraphs[5]);
-  for ( int i = 0; i < tgraphs_sim.size(); i++ ) {
-    myleg->AddEntry(tgraphs_sim[i]);
-  }
-  myleg->Draw();
+  // published limits
+  limit_curve argo("ArgoNeuT",kTeal);
+  limit_curve mini("MiniBooNE",kViolet);
+  limit_curve mill("milliQan demonstrator",kBlue);
+  limit_curve lsnd("LSND",kBlack);
+  limit_curve lhcc("LHC",kGray);
+  limit_curve slac("SLAC",kRed);
+  limit_curve sens("SENSEI",kOrange);
 
-  c1->SaveAs(Form("img/15ev_plot_%ikev.pdf",threshold));
-  delete c1;
+  mini.Draw("same");
+  mill.Draw("same");
+  lsnd.Draw("same");
+  lhcc.Draw("same");
+  slac.Draw("same");
+  argo.Draw("same");
+  sens.Draw("same");
+
+  // own limits (microboone)
+  limit_curve syst("def-th_no-syst",kGreen+1,false);
+  syst.SetLineStyle(1);
+  syst.SetTitle("#muBooNE normal thresholds (no systematics)");
+  syst.Draw("same LF");
+  limit_curve no_syst("def-th_full-syst",kGreen+2,false);
+  no_syst.SetLineStyle(2);
+  no_syst.SetTitle("#muBooNE normal thresholds (with full systematics)");
+  no_syst.Draw("same LF");
+  
+  
+  // legend
+  TLegend *myleg = new TLegend(0.6,0.15,0.9,0.5);
+  myleg->AddEntry(argo.get_tgraph());
+  myleg->AddEntry(mini.get_tgraph());
+  myleg->AddEntry(mill.get_tgraph());
+  myleg->AddEntry(lsnd.get_tgraph());
+  myleg->AddEntry(lhcc.get_tgraph());
+  myleg->AddEntry(slac.get_tgraph());
+  myleg->AddEntry(sens.get_tgraph());
+  myleg->AddEntry(syst.get_tgraph());
+  myleg->AddEntry(no_syst.get_tgraph());
+  myleg->Draw("same");
+
+  gStyle->SetOptStat(0);
+  c1->SaveAs("img/limits.pdf");
+
   
 }
-void fill_tgraph_vector(TString dir,   std::vector<TGraph *> &tgraphs)
+
+// class member implementation
+limit_curve::limit_curve(){};
+limit_curve::limit_curve(TString exp, Color_t col = kBlack, Bool_t pub = true)
 {
-  double nhits {1.};
-  //const char* dir = "/uboone/app/users/arellano/bdt_mcp/mass_scan/sensitivity/published/";
-  void *dirpointer = gSystem->OpenDirectory(dir);
-  std::vector<TString> full_files;
-  std::vector<TString> files;
-  std::vector<TString> experiments;
-  const char *entry;
-  TString dir_iteration;
-  while ( ( entry = (char*)gSystem->GetDirEntry(dirpointer))) {
-    dir_iteration = entry;
-    if ( dir_iteration.EndsWith(".") || dir_iteration == "README.txt" ) {
-      continue;
-    }
-    full_files.push_back(dir+dir_iteration);
-    files.push_back(dir_iteration);
-    experiments.push_back(dir_iteration.ReplaceAll(".dat",""));
-    std::cout << dir+dir_iteration << std::endl;
+  experiment = exp;
+  published = pub;
+  TString filename;
+  if ( published ) {
+    filename = "published/"+exp+".dat";
+  } else {
+    filename = "limits_apr_23/"+exp+".txt";
   }
-  std::cout << "Found limits for the following experiments:" << std::endl;
-  std::cout << experiments.size() << std::endl;
-  for ( int i = 0; i < experiments.size(); i++ ) {
-    std::cout << experiments[i] << std::endl;
-  }
-  std::vector<std::vector<Double_t>> experimentsx;
-  std::vector<std::vector<Double_t>> experimentsy;
-  std::vector<Double_t> datax;
-  std::vector<Double_t> datay;
-    for ( int i = 0; i < files.size(); i++ ) {
-    Double_t x,y;
-    std::cout << "opening file " << full_files[i] << std::endl;
-    ifstream in;
-    in.open(full_files[i]);
-    datax.clear();
-    datay.clear();
-    while (1) {
-      in >> x >> y;
-      //std::cout << x << " " << y << std::endl;
-      datax.push_back(x);
-      datay.push_back(y);
-      if (!in.good()) break;
-    }
-    experimentsx.push_back(datax);
-    experimentsy.push_back(datay);
-  }
-  std::cout << experimentsx[0].size() << std::endl;
+  dat_file.open(filename);
+  read_file();
+  set_color(col);
+  fill_tgraph();
+}
 
-  for ( int exp = 0; exp < experiments.size(); exp++ ) {
-    TGraph *gr = new TGraph(experimentsx[exp].size());
-    tgraphs.push_back(gr);
+void limit_curve::read_file()
+{
+  double x,y;
+  while (1) {
+    dat_file >> x >> y;
+    x_points.push_back(x);
+    y_points.push_back(y);
+    if (!dat_file.good()) break;
   }
-
-  for ( int exp = 0; exp < experiments.size(); exp++ ) {
-    std::cout << "Graph " << experiments[exp] << std::endl;
-    if ( experiments[exp].Contains("2hits") ) { nhits = 2; }
-    if ( experiments[exp].Contains("3hits") ) { nhits = 3; }
-    if ( experiments[exp].Contains("4hits") ) { nhits = 4; }
-    if ( nhits != 1 ) { tgraphs[exp]->SetTitle(Form("%.0f hits",nhits)); }
-    if ( experiments[exp].Contains("no-syst") ) { tgraphs[exp]->SetTitle("#muBooNE normal thresholds (no systematics)"); }
-    if ( experiments[exp].Contains("with-bkg-syst") ) { tgraphs[exp]->SetTitle("#muBooNE normal thresholds (with bkg quadrature)"); }
-    if ( experiments[exp].Contains("full-syst") ) { tgraphs[exp]->SetTitle("#muBooNE normal thresholds (with full systematics)"); }
-    if ( experiments[exp].Contains("limit") ) { tgraphs[exp]->SetTitle(Form("%.0f hits (limit)",nhits)); }
-    std::cout << Form("using %.0f hits", nhits) << std::endl;
-    for ( int point = 0; point < experimentsx[exp].size(); point++ ){
-      tgraphs[exp]->SetPoint(point, experimentsx[exp][point], experimentsy[exp][point]);
-      std::cout << tgraphs[exp]->GetMinimum() << " " << tgraphs[exp]->GetMaximum() << std::endl;
-    }
+  if ( x_points.size() != y_points.size() ) {
+    std::cout << Form("ERROR: sizes of x and y don't match, not filling exp %s",experiment.Data()) << std::endl;
+  } else {
+    npoints = x_points.size();
+  }
+  limit = new TGraph(npoints);
+  if ( x_points.size() <2 ) {
+    std::cout << Form("WARNING: Empty data for exp %s",experiment.Data()) << std::endl;
   }
 }
+
+
+void limit_curve::fill_tgraph()
+{
+  for ( int i = 0; i < x_points.size(); i++ ) {
+    limit->SetPoint(i,x_points[i],y_points[i]);
+  }
+  limit->SetLineColor(color);
+  limit->SetLineWidth(3);
+  limit->SetTitle(experiment);
+  if ( !published ) { // own limits filled
+    limit->SetFillColorAlpha(color,0.2);
+  }
+}
+
+void limit_curve::print_tgraph()
+{
+  for ( int i = 0; i < npoints; i++ ) {
+    std::cout << limit->GetPointX(i) << "  " << limit->GetPointY(i) << std::endl;
+  }
+}
+
